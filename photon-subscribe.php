0348dc876561c7885211c4363e82f08e2c3626b1
i-am-phi <phi@i-am-phi.net>
2020-09-01 18:11:59 -0700

    add css to pipeline

--- photon-subscribe.php
+++ photon-subscribe.php
@@ -81,7 +81,7 @@ class PhotonSubscribePlugin extends Plugin
 
       // styles
       $css = 'plugin://photon-subscribe/assets/subscribe.css';
-      $assets->addCss($css, 100, false, 'photon-plugins' );
+      $assets->addCss($css, 100, 'pipeline', 'photon-plugins' );
 
       // only load the vars if this datatype page
       if ($page->template() == 'subscribe')
